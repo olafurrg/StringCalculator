9ab8247eb0f4894e83ff6aa30eaeab31dcc8f53a
olafurrg <olafurg16@ru.is>
2018-10-12 12:33:22 +0000

    better testing for 1 input

--- StringCalculator.js
+++ StringCalculator.js
@@ -3,6 +3,14 @@ function Add(numbers){
 	if(numbers == ""){
 		return "0";
 	}
+	if(!numbers.includes(",")){
+		return numbers;
+	}
+	/*if(numbers.length == 2){
+		var toRetrun = parseInt(numbers[0], 10) + parseInt(numbers[1], 10);
+		return toRetrun.toString();
+
+	}*/
 
 }
 
